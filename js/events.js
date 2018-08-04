94951496c2b828cea2557bfe27c64c8f50c597b3
karen S <buzz.kyt@gmail.com>
2018-08-04 21:20:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,5 @@
 function getIt() {
+<<<<<<< HEAD
   $('p').on("click", function() {
   alert('Hey!');
 });
@@ -18,12 +19,39 @@ function submitIt(){
 
 function pressIt(){
   $('#typing').on("keydown", function(event) {
+=======
+  return "Hey!";
+}
+  
+function frameIt() {
+  $('img').addClass("tasty");
+  return;
+}
+
+function submitIt(){
+  alert("Your form is going to be submitted now.");
+  return;
+}
+
+$('p').on("click", getIt());
+ 
+$('img').on("load", frameIt());
+
+$('#typing').on("keypress", function pressIt(event) {
+  event = event.which;
+>>>>>>> d491ab74d1b202bfd7b4c18ce746f79696bf7d4f
   if (event.which == 71) {
   alert("G!");
     }
 });
+<<<<<<< HEAD
 }
 
+=======
+
+$('input:last').on("click", submitIt());
+
+>>>>>>> d491ab74d1b202bfd7b4c18ce746f79696bf7d4f
 $(document).ready(function(){
  
  getIt();
